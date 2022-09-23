d508512fae0987280ce17e8a4391ba88e914f365
kitem1tsUri <huslenbasketball@gmail.com>
2022-09-23 18:32:16 +0800

    hi

--- margudai /weather.js
+++ margudai /weather.js
@@ -9,7 +9,6 @@ const getData = async (city) => {
       `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=0a9b1d70e2d449a7fcadd8c7ceaaade5`
     );
     const info = await data.json();
-    document.getElementsByClassName('number').
     console.log(info); scrollBy
   };
   
